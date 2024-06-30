9c0819c290ce937e888658f84165b2c93db71ef8
Mohamed Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>
2024-06-30 05:38:39 +0300

    chore: Update login screen to use Gluestack UI components and dependencies

--- app/index.js
+++ app/index.js
@@ -2,11 +2,14 @@ import { Button, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { router } from "expo-router";
 import LoginScreen from "./screens/auth/loginScreen";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
 
 
 const HomeScreen = () => {
     return (
-        <LoginScreen />
+        <GluestackUIProvider>
+            <LoginScreen />
+        </GluestackUIProvider>
     );
 };
 export default HomeScreen;
--- app/screens/auth/loginScreen.js
+++ app/screens/auth/loginScreen.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View, TextInput, Button } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Image } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginUser } from '../../redux/slices/userSlice';
 import { router } from 'expo-router';
+
+const logo = require('../../../assets/logo.png');
 
 const LoginScreen = () => {
     const dispatch = useDispatch();
@@ -15,28 +17,62 @@ const LoginScreen = () => {
     };
 
     useEffect(() => {
-        console.log(user);
         if (user) {
             router.push('screens/testScreen');
         }
     }, [user]);
 
     return (
-        <View>
+        <View style={styles.container}>
+            <Image source={logo} style={styles.image} />
             <TextInput
+                style={styles.input}
                 placeholder="Email"
                 value={email}
                 onChangeText={setEmail}
             />
             <TextInput
+                style={styles.input}
                 placeholder="Password"
                 secureTextEntry
                 value={password}
                 onChangeText={setPassword}
             />
-            <Button title="Login" onPress={handleLogin} />
+            <Button title="Login" color={"#2acaac"} style={styles.button} onPress={handleLogin} />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    input: {
+        width: '80%',
+        height: 70,
+        borderRadius: 15,
+        borderWidth: 3,
+        borderColor: '#2acaac',
+        marginBottom: 10,
+        paddingHorizontal: 10,
+        fontSize: 20,
+    },
+    button: {
+        width: '80%',
+        height: 70,
+        borderRadius: 15,
+        backgroundColor: '#2acaac',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: 200,
+        height: 200,
+        marginBottom: 20,
+        objectFit: 'contain'
+    },
+});
 
 export default LoginScreen;
