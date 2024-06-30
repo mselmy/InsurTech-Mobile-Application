a4f8f4af170a87e3c590f82919d70d0ea8426458
Mohamed Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>
2024-06-30 03:07:31 +0300

    chore: Custom splash screen

--- app/_layout.js
+++ app/_layout.js
@@ -1,28 +1,35 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
-import { Colors } from 'react-native/Libraries/NewAppScreen'
 
 const _layout = () => {
-  return (
-      <Stack styles={styles.stackStyle}>
-        <Stack.Screen
-            name="index"
-            options={{
-                headerTitle: "Home",
-                headerTitleAlign: "center",
-            }}
-        />
+return (
+    <Stack styles={styles.stackStyle} initialRouteName='screens/auth/loginScreen'>
+            <Stack.Screen
+                    name="index"
+                    options={{
+                            headerTitle: "Home",
+                            headerTitleAlign: "center",
+                    }}
+            />
 
-          <Stack.Screen
-              name="screens/testScreen"
-              options={{
-                  headerTitle: "Test",
-                  headerTitleAlign: "center",
-              }}
-          />
+                <Stack.Screen
+                        name="screens/testScreen"
+                        options={{
+                                headerTitle: "Test",
+                                headerTitleAlign: "center",
+                        }}
+                />
+
+                <Stack.Screen
+                        name="screens/auth/loginScreen"
+                        options={{
+                                headerTitle: "Login",
+                                headerTitleAlign: "center",
+                        }}
+                />
     </Stack>
-  )
+)
 }
 
 export default _layout
---
+++ app/screens/auth/loginScreen.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button } from 'react-native';
+
+const LoginScreen = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        // Add your login logic here
+    };
+
+    return (
+        <View>
+            <TextInput
+                placeholder="Email"
+                value={email}
+                onChangeText={setEmail}
+            />
+            <TextInput
+                placeholder="Password"
+                secureTextEntry
+                value={password}
+                onChangeText={setPassword}
+            />
+            <Button title="Login" onPress={handleLogin} />
+        </View>
+    );
+};
+
+export default LoginScreen;
