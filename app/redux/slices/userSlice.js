0060fc8b0f73582de97377029e2d1defcc25fcb9
Mohamed Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>
2024-07-02 13:18:15 +0300

    chore: Update API endpoints to use local development server

--- app/redux/slices/userSlice.js
+++ app/redux/slices/userSlice.js
@@ -6,7 +6,7 @@ export const loginUser = createAsyncThunk(
     async (userData, thunkAPI) => {
         try {
             // Make API call to login user
-            const response = await fetch('https://insurtechapis.azurewebsites.net/api/Account/Login', {
+            const response = await fetch('http://localhost:5028/api/Account/Login', {
                 method: 'POST',
                 body: JSON.stringify(userData),
                 headers: {
@@ -32,7 +32,7 @@ export const registerUser = createAsyncThunk(
     async (userData, thunkAPI) => {
         try {
             // Make API call to register user
-            const response = await fetch('https://insurtechapis.azurewebsites.net/api/Acount/RegisterCustomer', {
+            const response = await fetch('http://localhost:5028/api/Acount/RegisterCustomer', {
                 method: 'POST',
                 body: JSON.stringify(userData),
                 headers: {
